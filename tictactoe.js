7de69c13fcad2bdc67e318e055f85eb37269c701
Nicole Pasquale <NSquale212@gmail.com>
2019-10-01 10:06:51 -0500

    Add reset function

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,12 @@
 let cells = document.querySelectorAll('.row > div');
 let currentPlayer = '🔥'; 
-let moveCounter = document.querySelector('.row > div');
+let winner = document.querySelector('#winner');
 let counter = 0;
+let isGameOver = false;
+let resetButton = document.querySelector('.reset');
 
-moveCounter.addEventListener('click', function() {
-	counter++;
-	console.log(counter);
+resetButton.addEventListener('click', function() {
+	resetGame();
 });
 
 for (let i = 0; i < cells.length; i++) {
@@ -14,11 +15,17 @@ for (let i = 0; i < cells.length; i++) {
 
 function cellClicked(e) {
 	let cell = e.target;
+	if(isGameOver === true) {
+		resetGame();
+		return;
+	}
 	if(cell.textContent !== '') {
 		return;
 	}
+
 	// draw current player value
 	cell.textContent = currentPlayer;
+	counter++;
 	checkForWin();
 	togglePlayer();
 }
@@ -34,23 +41,46 @@ function togglePlayer() {
 
 function checkForWin() {
 	if(cells[0].textContent === currentPlayer && cells[1].textContent === currentPlayer && cells[2].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
 	} else if(cells[3].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[5].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
 	} else if(cells[6].textContent === currentPlayer && cells[7].textContent === currentPlayer && cells[8].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
 	} else if(cells[0].textContent === currentPlayer && cells[3].textContent === currentPlayer && cells[6].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
 	} else if(cells[1].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[7].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
 	} else if(cells[2].textContent === currentPlayer && cells[5].textContent === currentPlayer && cells[8].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
 	} else if(cells[0].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[8].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
 	} else if(cells[2].textContent === currentPlayer && cells[4].textContent === currentPlayer && cells[6].textContent === currentPlayer) {
-		alert(`${currentPlayer} Wins!`);
-	} 
+		winner.textContent = `${currentPlayer} Wins!`;
+		isGameOver = true;
+	} else if(counter === 9) {
+		winner.textContent = 'Draw!';
+		isGameOver = true;
+	}
 }
+
+function resetGame() {
+	for (let i = 0; i < cells.length; i++) {
+		cells[i].textContent = '';
+	}
+	isGameOver = false;
+	counter = 0;
+	currentPlayer = '🔥';
+	winner.textContent = '';
+}
+
+
 
 
 
